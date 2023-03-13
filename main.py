f1902c184eb719c137127d0fb9eba21c48517f42
matthew <pseudoRoute@pm.me>
2023-03-12 23:59:53 -0700

    commented out

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from parameter.parameter import Parameter
-import numpy as np
+# from parameter.parameter import Parameter
+# import numpy as np
 
-def main():
-    data = np.random.rand(10, 10)
-    units = 'mm'
-    param = Parameter(data, units)
-    print(param)
-    print(param.si_units)
+# def main():
+#     data = np.random.rand(10, 10)
+#     units = 'mm'
+#     param = Parameter(data, units)
+#     print(param)
+#     print(param.si_units)
 
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
